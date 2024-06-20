4df36f040b41cb0f43ed253b8ca8d1a57ef3ae32
jensenmkaplan <jensenkaplan@proton.me>
2024-06-20 01:53:36 -0600

    changed formatting function for labels so that it no longer relies on a globl variable. replaced with lambda function. turns out lambdas are useful...

--- retail_analysis.py
+++ retail_analysis.py
@@ -1,31 +1,34 @@
 ### Jensen Kaplan
 ## Using a trending kaggle dataset as a playground to display
 ## analytical skills and data visualization techniques
+## ORIGINAL SOURCE: https://www.kaggle.com/datasets/sahilprajapati143/retail-analysis-large-dataset/code
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Function to calculate the absolute value of the percentage
 # Used to display the total revenue for each category
-def absolute_value_rev(val):
-    total = sum(revenue_by_category)
+def absolute_value_rev(val, total):
     return f'${val * total / 100:,.2f}'# Function to calculate the absolute value of the percentage
-
 
 
 # Load the dataset
 df = pd.read_csv('Online Sales Data.csv')
 
 # Print column names to explore the parameters of the dataset
-print(df.columns)
+print('##The dataset contains the following columns##')
+for i in df.columns:
+    print(i)
+
 
 # Group by 'Product Category' and sum the 'Total Revenue'
 revenue_by_category = df.groupby('Product Category')['Total Revenue'].sum()
 # print(revenue_by_category)
 
-# Create the pie chart
+# Create the pie chart dispalaying total revenue by product category
 plt.figure(figsize=(10, 6))
-plt.pie(revenue_by_category, labels=revenue_by_category.index, autopct=absolute_value_rev, startangle=140)
+total_revenue = sum(revenue_by_category)
+plt.pie(revenue_by_category, labels=revenue_by_category.index, autopct=lambda val: absolute_value_rev(val, total_revenue), startangle=140)
 # Adding title
 plt.title('Total Revenue by Product Category')
 # Equal aspect ratio ensures that pie is drawn as a circle.
