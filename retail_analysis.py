c7cdee8031833d3b433cc45e17c8bc28d31dab71
jensenmkaplan <jensenkaplan@proton.me>
2024-06-22 04:20:22 -0600

    first pie chart, then bar chart, now heat map.... diff wya sto visualize trends

--- retail_analysis.py
+++ retail_analysis.py
@@ -5,12 +5,13 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import  numpy as np
+import seaborn as sns
 
 # Function to calculate the absolute value of the percentage
 # Used to display the total revenue for each category
 def absolute_value_rev(val, total):
-    return f'${val * total / 100:,.2f}'# Function to calculate the absolute value of the percentage
-
+    return f'${val * total / 100:,.2f}  ({val:,.0f}%)'
 
 # Load the dataset
 df = pd.read_csv('Online Sales Data.csv')
@@ -25,11 +26,15 @@ for i in df.columns:
 revenue_by_category = df.groupby('Product Category')['Total Revenue'].sum()
 # print(revenue_by_category)
 
+# Calculate explode values - a small value for each category to explode them slightly
+explode_values = [0.1] * len(revenue_by_category)  # Adjust 0.1 to increase or decrease the explosion distance
+
+
 # Create the pie chart dispalaying total revenue by product category
 plt.figure(figsize=(10, 6))
 total_revenue = sum(revenue_by_category)
 total_revenue = sum(revenue_by_category)
-plt.pie(revenue_by_category, labels=revenue_by_category.index, autopct=lambda val: absolute_value_rev(val, total_revenue), startangle=140)# Adding title
+plt.pie(revenue_by_category, labels=revenue_by_category.index, autopct=lambda val: absolute_value_rev(val, total_revenue), startangle=140, explode=explode_values)# Adding title
 plt.title('Total Revenue by Product Category')
 # Equal aspect ratio ensures that pie is drawn as a circle.
 plt.axis('equal')
@@ -38,7 +43,6 @@ plt.show()
 
 # electronics_sold = df[df['Product Category']]
 electronics_sold = df[df['Product Category'] == 'Electronics']['Units Sold'].sum()
-print(electronics_sold)
 
 # Group the data by product category and sum the units sold
 counts_by_category = df.groupby('Product Category')['Units Sold'].sum()
@@ -55,4 +59,21 @@ plt.xlabel('Product Category')
 plt.ylabel('Units Sold')
 
 # Display the chart
+plt.show()
+
+
+# Now exploring sales by region and product category
+grouped_data = df.groupby(['Region', 'Product Category'])['Total Revenue'].sum().reset_index()
+
+# Pivot the data for heatmap visualization
+pivot_table = grouped_data.pivot(index="Region", columns="Product Category", values="Total Revenue")
+# Convert pivot table values to formatted currency strings
+formatted_pivot_table = pivot_table.applymap(lambda x: f"${x:,.0f}")
+
+# Create a heatmap
+plt.figure(figsize=(10, 8))
+sns.heatmap(pivot_table, annot=formatted_pivot_table, fmt="", cmap="YlGnBu")
+plt.title('Revenue by Product Category across Regions')
+plt.ylabel('Region')
+plt.xlabel('Product Category')
 plt.show()
